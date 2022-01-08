480dece3acf114dae611f72612e90312376f3d29
Bruce Dickson <95632543+B-Dickinson@users.noreply.github.com>
2022-01-08 17:13:36 -0300

    add files

---
+++ exercicios/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['Maria','Fátima'];
+const filhos = ['Mário','Fábio'];
+const todos = filhas.concat(filhos);
+
+console.log('Família: \n');
+
+todos.forEach(filhos => console.log(filhos));
